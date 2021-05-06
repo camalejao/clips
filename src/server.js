7399b19045caeaa277dcf5a09d395d380a3c2de3
João Falcão <jvfsl@ic.ufal.br>
2021-05-06 01:32:28 -0300

    serving react files

--- src/server.js
+++ src/server.js
@@ -6,8 +6,8 @@ const twitchService = require("./twitchService");
 const app = express();
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "..", "public")));
-app.set("views", path.join(__dirname, "..", "public"));
+app.use(express.static(path.join(__dirname, "..", "client/build")));
+app.set("views", path.join(__dirname, "..", "client/build"));
 app.engine("html", require("ejs").renderFile);
 app.set("view engine", "html");
 
